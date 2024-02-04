393302de498ae628a68b96eb1b533fb369bb7bd1
Viswanadh001 <kviswanadhacharan@gmail.com>
2024-02-04 17:40:51 +0530

    't'

--- Collection_Framework/HashSet/HasahSet_2.java
+++ Collection_Framework/HashSet/HasahSet_2.java
@@ -11,15 +11,27 @@ public class HasahSet_2 {
 		ids.add(103);
 		ids.add(10);
 		ids.add(30);
+		
+		System.out.println("IDS:");
 		System.out.println(ids);
 		//Collections.sort(ids); sorting is not applicable in Hashset
+		
+		for(Integer id : ids) {
+			System.out.println(id);
+		}
+		System.out.println(" ");
 	
 		HashSet<String> names = new HashSet<String>();
 		names.add("Pspk");
 		names.add("Thala");
 		names.add("NTR");
 		names.add("NBK");
+		System.out.println("Names :");
 		System.out.println(names);
+		
+		for (String name:names) {
+			System.out.println(name);
+		}
 	 }
 	}
 
